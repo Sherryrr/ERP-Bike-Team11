d64251bc97633cf5f9eefb959632ec5c07d7543f
Shehryaar Sarfraz <72866895+Sherryrr@users.noreply.github.com>
2023-05-25 22:47:24 +0500

    Update MaterialService.java
    Added multi-line comments that describe the purpose of MaterialService.java as the main class for the ERP Bike System.
    
    It also includes a single-line comment for the deleteMaterial method, explaining its purpose in handling the deletion of a material with the specified ID from the database.
    
    Inside the deleteMaterial method, there are single-line comments that describe the purpose of the decision-making structures. These comments clarify that the if statement checks whether the material with the specified ID exists in the database. Additionally, the comment for the for loop highlights its purpose of iterating over the list of MaterialRawMaterials to retrieve the corresponding RawMaterialRequestDto objects.

--- MaterialService.java
+++ MaterialService.java
@@ -1,3 +1,12 @@
+
+/*
+ * This class represents the MaterialService, which is a service layer for managing materials in an application.
+ * It provides various methods for creating, retrieving, updating, and deleting materials, as well as retrieving
+ * information about raw materials associated with materials. The class interacts with the MaterialRepository and
+ * MaterialRawMaterialRepository to perform database operations related to materials and their associations with raw
+ * materials. Additionally, it utilizes the RawMaterialService and LogService for specific functionality.
+ */
+
 package com.soen390.team11.service;
 
 import com.soen390.team11.constant.LogTypes;
@@ -75,7 +84,10 @@ public class MaterialService {
      * @return success message
      * @throws Exception Thrown exception if there is an invalid ID
      */
+    
+    // Deletes a material with the specified ID from the database.
     public String deleteMaterial(String id) throws Exception {
+        // Checks if the material with the specified ID exists in the database.
         if(getMaterialById(id)==null) {
             throw new Exception("invalid id");
         }
@@ -121,9 +133,12 @@ public class MaterialService {
      * @param materialid The Material's ID
      * @return List of Raw Materials
      */
+    
+    // Retrieves a list of raw materials associated with a specific material ID.
     public List<RawMaterialRequestDto> getAllMaterialRawMaterial(String materialid){
         List<RawMaterialRequestDto> rawMaterial= new ArrayList<>();
         List<MaterialRawMaterials> MaterialRawMaterialsList = materialrawmaterialRepository.findByMaterialRawMaterialsIdRawmaterialid(materialid);
+        // Iterates over the list of MaterialRawMaterials to retrieve the corresponding RawMaterialRequestDto objects.
         for(MaterialRawMaterials materialRawMaterials : MaterialRawMaterialsList){
             rawMaterial.add(rawmaterialService.getRawMaterialById(materialRawMaterials.getMaterialRawMaterialsId().getRawmaterialid()));
         }
